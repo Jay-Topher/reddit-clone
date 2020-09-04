0613494f5e64b22ea7b01c5ad39f77bcce3de3e4
Jones Ogolo <jay.topher11@gmail.com>
2020-09-04 10:44:05 +0100

    added express and connected graphql

--- reddit-server/src/index.ts
+++ reddit-server/src/index.ts
@@ -1,14 +1,34 @@
-import {MikroORM} from '@mikro-orm/core'
-import { __prod__ } from './constants'
-import { Post } from './enttites/Post'
-import microConfig from './mikro-orm.config'
+import "reflect-metadata";
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import microConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
+import { UserResolver } from "./resolvers/user";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
 
-  const post = orm.em.create(Post, {title: "My first post"})
-  await orm.em.persistAndFlush(post);
-}
+  // const post = orm.em.create(Post, { title: "My second Post" })
+  // await orm.em.persistAndFlush(post);
 
-main().catch(err => console.log(err))
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver, UserResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => console.log("Server started on localhost:4000"));
+};
+
+main().catch((err) => console.log(err));
