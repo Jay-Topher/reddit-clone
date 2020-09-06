7b2ba955c53603b1b17bd5c6fef53053c96ab910
Jones Ogolo <jay.topher11@gmail.com>
2020-09-06 18:50:07 +0100

    created navbar

---
+++ reddit-client/src/components/Navbar.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Box, Link, Flex, Button } from "@chakra-ui/core";
+import NextLink from "next/link";
+import { useMeQuery } from "../generated/graphql";
+
+interface NavbarProps {}
+
+const Navbar: React.FC<NavbarProps> = ({}) => {
+  const [{ data, fetching }] = useMeQuery();
+  let body = null;
+  if (fetching) {
+    body = null;
+  } else if (!data?.me) {
+    body = (
+      <>
+        <NextLink href="/login">
+          <Link mr={2}>Login</Link>
+        </NextLink>
+        <NextLink href="/register">
+          <Link>Register</Link>
+        </NextLink>
+      </>
+    );
+  } else {
+    body = (
+      <Flex>
+        <Box mr={2}>{data.me.username}</Box>
+        <Button variant="link">Logout</Button>
+      </Flex>
+    );
+  }
+  return (
+    <Flex p={4} bg="tomato">
+      <Box ml="auto">{body}</Box>
+    </Flex>
+  );
+};
+
+export default Navbar;
