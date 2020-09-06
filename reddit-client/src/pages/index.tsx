b8be317e0295e87f5b8db05f0c448316281e890f
Jones Ogolo <jay.topher11@gmail.com>
2020-09-06 18:50:25 +0100

    added navbar to index

--- reddit-client/src/pages/index.tsx
+++ reddit-client/src/pages/index.tsx
@@ -1,5 +1,10 @@
+import Navbar from "../components/Navbar";
 
-
-const Index = () => <div>Hello</div>;
+const Index = () => (
+  <>
+    <Navbar />
+    <div>Hello</div>
+  </>
+);
 
 export default Index;
