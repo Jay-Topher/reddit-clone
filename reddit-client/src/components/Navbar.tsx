7f9da6ff8ac8a75c863e5d8af65c8d75f9dbb319
Jones Ogolo <jay.topher11@gmail.com>
2020-09-08 19:18:43 +0100

    added logout to navbar

--- reddit-client/src/components/Navbar.tsx
+++ reddit-client/src/components/Navbar.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Box, Link, Flex, Button } from "@chakra-ui/core";
 import NextLink from "next/link";
-import { useMeQuery } from "../generated/graphql";
+import { useMeQuery, useLogoutMutation } from "../generated/graphql";
 
 interface NavbarProps {}
 
 const Navbar: React.FC<NavbarProps> = ({}) => {
+  const [{ fetching: logoutFetching }, logout] = useLogoutMutation();
   const [{ data, fetching }] = useMeQuery();
   let body = null;
   if (fetching) {
@@ -25,12 +26,18 @@ const Navbar: React.FC<NavbarProps> = ({}) => {
     body = (
       <Flex>
         <Box mr={2}>{data.me.username}</Box>
-        <Button variant="link">Logout</Button>
+        <Button
+          variant="link"
+          onClick={() => logout()}
+          isLoading={logoutFetching}
+        >
+          Logout
+        </Button>
       </Flex>
     );
   }
   return (
-    <Flex p={4} bg="tomato">
+    <Flex p={4} bg="khaki">
       <Box ml="auto">{body}</Box>
     </Flex>
   );
