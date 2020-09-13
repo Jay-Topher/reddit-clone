93ae33a6c650e7eaa9283eceeb61f137828656b4
Jones Ogolo <jay.topher11@gmail.com>
2020-09-13 01:50:07 +0100

    replaced mikroorm to typeorm

--- reddit-server/src/entites/User.ts
+++ reddit-server/src/entites/User.ts
@@ -1,29 +1,33 @@
-import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
 import { ObjectType, Field } from "type-graphql";
+import { Entity, PrimaryGeneratedColumn, Column, UpdateDateColumn, CreateDateColumn, BaseEntity, OneToMany } from "typeorm";
+import { Post } from "./Post";
 
 @ObjectType()
 @Entity()
-export class User {
+export class User extends BaseEntity {
   @Field()
-  @PrimaryKey()
+  @PrimaryGeneratedColumn()
   id!: number;
 
   @Field(() => String)
-  @Property({ type: "text", unique: true })
+  @Column({unique: true})
   username!: string;
 
-  @Property({ type: "text" })
+  @Column()
   password!: string;
 
   @Field()
-  @Property({ type: "text", unique: true })
+  @Column({unique: true})
   email!: string;
 
-  @Field(() => String)
-  @Property({ type: "date", onUpdate: () => new Date() })
-  updatedAt = new Date();
+  @OneToMany(() => Post, post => post.creator)
+  posts: Post[]
 
   @Field(() => String)
-  @Property({ type: "date" })
-  createdAt = new Date();
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @Field(() => String)
+  @CreateDateColumn()
+  createdAt: Date;
 }
