86209cf9f4faa6fc9a8be849a1511b08897dee22
Jones Ogolo <jay.topher11@gmail.com>
2020-09-22 09:29:05 +0100

    added pagination logic to posts

--- reddit-client/src/pages/index.tsx
+++ reddit-client/src/pages/index.tsx
@@ -3,23 +3,63 @@ import { withUrqlClient } from "next-urql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 import { usePostsQuery } from "../generated/graphql";
 import Layout from "../components/Layout";
-import { Link } from "@chakra-ui/core";
+import { Button, Flex, Link } from "@chakra-ui/core";
 import NextLink from "next/link";
+import { Stack, Box, Heading, Text } from "@chakra-ui/core";
+import React, { useState } from "react";
 
 const Index = () => {
-  const [{ data, fetching }] = usePostsQuery();
+  const [variables, setVariables] = useState({
+    limit: 10,
+    cursor: null as null | string,
+  });
+  const [{ data, fetching }] = usePostsQuery({
+    variables,
+  });
+
+  if (!fetching && !data) {
+    return <div>No posts found, please try again later</div>;
+  }
+
   return (
     <Layout>
-      <NextLink href="/create-post">
-        <Link>Create Post</Link>
-      </NextLink>
-      <div>Hello</div>
+      <Flex alignItems="center">
+        <Heading>LiReddit</Heading>
+        <NextLink href="/create-post">
+          <Link ml="auto">Create Post</Link>
+        </NextLink>
+      </Flex>
+
       <br />
-      {!data ? (
+      {!data && fetching ? (
         <div>loading...</div>
       ) : (
-        data.posts.map((p) => <div key={p.id}>{p.title}</div>)
+        <Stack>
+          {data!.posts.map((p) => (
+            <Box p={5} my={4} shadow="md" borderWidth="1px" key={p.id}>
+              <Heading fontSize="xl">{p.title}</Heading>
+              <Text mt={4}>{p.textSnippet}...</Text>
+            </Box>
+          ))}
+        </Stack>
       )}
+      {data ? (
+        <Flex>
+          <Button
+            m="auto"
+            my={4}
+            isLoading={fetching}
+            onClick={() => {
+              setVariables({
+                limit: variables.limit,
+                cursor: data.posts[data.posts.length - 1].createdAt,
+              });
+            }}
+          >
+            Load more
+          </Button>
+        </Flex>
+      ) : null}
     </Layout>
   );
 };
