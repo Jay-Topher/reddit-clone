5e79c7aebb754e545489be4f03a726635694684a
Jones Ogolo <jay.topher11@gmail.com>
2020-09-22 09:25:13 +0100

    used cursor pagination

--- reddit-server/src/resolvers/post.ts
+++ reddit-server/src/resolvers/post.ts
@@ -8,10 +8,13 @@ import {
   Field,
   Ctx,
   UseMiddleware,
+  Root,
+  FieldResolver,
 } from "type-graphql";
 import { Post } from "../entites/Post";
 import { MyContext } from "../types";
 import { isAuth } from "../middleware/isAuth";
+import { getConnection } from "typeorm";
 
 @InputType()
 class PostInput {
@@ -22,12 +25,33 @@ class PostInput {
   text: string;
 }
 
-@Resolver()
+@Resolver(Post)
 export class PostResolver {
+  @FieldResolver(() => String)
+  textSnippet(@Root() root: Post) {
+    return root.text.slice(0, 50);
+  }
+
   @Query(() => [Post])
-  async posts(): Promise<Post[]> {
+  async posts(
+    @Arg("limit", () => Int) limit: number,
+    @Arg("cursor", () => String, { nullable: true }) cursor: string | null
+  ): Promise<Post[]> {
     // await sleep(3000);
-    return Post.find();
+    const realLimit = Math.min(50, limit);
+    const qb = getConnection()
+      .getRepository(Post)
+      .createQueryBuilder("p")
+      .orderBy('"createdAt"', "DESC")
+      .take(realLimit);
+
+    if (cursor) {
+      qb.where('"createdAt" < :cursor', {
+        cursor: new Date(parseInt(cursor)),
+      });
+    }
+
+    return qb.getMany();
   }
 
   @Query(() => Post, { nullable: true })
